9a0e4ac059b9399d4cb1d250e6e90dee84097527
maug0006 <maug0006@student.monash.edu>
2024-11-10 13:23:53 +1100

    Initial commit

---
+++ src/main/java/com/mitchellaugustini/bingescale/BingescaleApplication.java
@@ -0,0 +1,13 @@
+package com.mitchellaugustini.bingescale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingescaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingescaleApplication.class, args);
+	}
+
+}
